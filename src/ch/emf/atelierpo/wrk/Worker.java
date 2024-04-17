1db017ac2fabb43433a574934e7aa23cac9fa74a
alilis01 <alilis01@WSTEMFA44-09.stu.net.fr.ch>
2024-04-17 09:01:04 +0200

    réalisation de la javadoc

--- src/ch/emf/atelierpo/wrk/Worker.java
+++ src/ch/emf/atelierpo/wrk/Worker.java
@@ -1,13 +1,21 @@
 package ch.emf.atelierpo.wrk;
 
-
 /**
  *
  * @author Mario Ramalho(EMF - Fribourg)
  */
 public class Worker {
 
-
+    /**
+     * valide un numéro de téléphone donnée en paramètre selon les critères
+     * suivant :
+     *
+     * @param prefix le préfix du numéro de téléphone permettant uniquement les
+     * cas suivants : +4176, +4177, +4178 et +4179
+     * @param numero le numéro devant contenir des caractères 0-9 sous le format
+     * "xxx xx xx" séparé uniquement d'espaces
+     * @return un boolean représentant la validité du numéro de téléphone
+     */
     public boolean controleNumeroTelMobile(String prefix, String numero) {
         return false;
     }
