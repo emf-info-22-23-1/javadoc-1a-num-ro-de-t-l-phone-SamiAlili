0cc2fa29a67ea25e21c04af4818fbfe23c8afc2f
RibeiroJ01 <RibeiroJ01@WSTEMFA44-15.stu.net.fr.ch>
2024-04-17 13:02:05 +0200

    Correction

--- src/ch/emf/atelierpo/wrk/Worker.java
+++ src/ch/emf/atelierpo/wrk/Worker.java
@@ -12,13 +12,27 @@ public class Worker {
      *
      * @param prefix le préfix du numéro de téléphone permettant uniquement les
      * cas suivants : <b>+4176, +4177, +4178 et +4179</b>
-     * @param numero le numéro devant contenir des caractères <b>0-9</b> sous le format
+     * @param numero le numéro devant contenir des caractères <b>0-9</b> sous le
+     * format
      * <b>"xxx xx xx"</b> séparé uniquement <b>d'espaces</b>
      *
      * @return un boolean représentant la validité du numéro de téléphone
      */
     public boolean controleNumeroTelMobile(String prefix, String numero) {
-        return false;
-    }
+        boolean resultat = false;
+        
+        if (prefix != null && numero != null) {
 
+            // Vérification du préfixe
+            if (prefix.matches("\\+417[6-9]")) {
+                resultat = true;
+            }
+            if (numero.matches("[0-9]{3}\\s[0-9]{2}\\s[0-9]{2}")) {
+                resultat = true;
+            }
+
+        }
+
+        return resultat;
+    }
 }
